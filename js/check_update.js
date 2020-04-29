ff5f7608116600ee91e70ab89600a63ccb8f6e7d
DewGew <pierlar@hotmail.com>
2020-04-29 09:04:49 +0200

    change github URL:s

--- js/check_update.js
+++ js/check_update.js
@@ -3,13 +3,13 @@ var branch = "beta";
 setTimeout(update, 5000);
 function update() {
     $.ajax({
-        url: "https://raw.githubusercontent.com/EdddieN/machinon-domoticz_theme/" + branch + "/theme.json",
+        url: "https://raw.githubusercontent.com/domoticz/machinon/" + branch + "/theme.json",
         async: false,
         dataType: "json",
         success: function(data) {
             gitVersion = data.version;
             if (theme.version !== gitVersion) {
-                newVersionText = "Machinon version " + data.version + " " + language.is_available + '! <a href="https://github.com/EdddieN/machinon-domoticz_theme/tree/' + branch + '" target="_blank">' + language.click_here + "</a>";
+                newVersionText = "Machinon version " + data.version + " " + language.is_available + '! <a href="https://github.com/domoticz/machinon/tree/' + branch + '" target="_blank">' + language.click_here + "</a>";
                 notify(newVersionText, 0);
             }
         }
