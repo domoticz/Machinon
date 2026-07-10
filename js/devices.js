0ee3ae3d3e8bc8e3e95c414f6f1fc831f5533f15
Rouzax <GitHub@mgdn.nl>
2026-07-10 18:59:15 +0200

    fix(devices): close the :not() in the mobile selector enhancement
    $("#id:not(.ui-widget") is an invalid selector: jQuery throws
    'unrecognized expression' the first time a mobile client renders a
    selector-level device, killing the enhancement pass that called it.
    Surfaced while auditing the polling loop in ajaxSuccessCallback, which
    invokes setDevicesNativeSelectorForMobile on every tick.
    
    Not demonstrated live: the test instance runs desktop viewports, where
    isMobile gates the function off; verified by jQuery selector semantics
    and syntax check only.

--- js/devices.js
+++ js/devices.js
@@ -3,7 +3,7 @@ function setDevicesNativeSelectorForMobile() {
     $(".selectorlevels span.ui-selectmenu-button").each(function() {
         $(this).hide();
         var selectorId = $(this).attr("id").split("-", 1)[0];
-        $("#" + selectorId + ":not(.ui-widget").on("change", function(e) {
+        $("#" + selectorId + ":not(.ui-widget)").on("change", function(e) {
             var selected = $(this).children("option:selected");
             SwitchSelectorLevel($(this).attr("data-idx"), selected.text(), selected.val());
         });
